e1bac06aab7aa8f9dd6fa100e1f49cec5a1e2159
mevunidesaram <mevunid97@gmail.com>
2021-08-17 17:33:23 +0530

    blender to opengl-1

---
+++ cmake-build-debug/CMakeFiles/blender2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/blender2.dir/main.cpp.o"
+  "blender2"
+  "blender2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/blender2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
